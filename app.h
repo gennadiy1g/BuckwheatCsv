ec7091de7e4979cf5f88fb570e54bac9e4882542
Gennadiy <gennadiy1g@gmx.com>
2019-12-03 12:17:36 -0500

    Introduced new header file app.h.

---
+++ app.h
@@ -0,0 +1,26 @@
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include <wx/grid.h>
+
+class App : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class AppFrame : public wxFrame
+{
+public:
+    AppFrame();
+
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
+enum { ID_Hello = 1 };
--- main.cpp
+++ main.cpp
@@ -1,28 +1,4 @@
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
-
-#ifndef WX_PRECOMP
-#include <wx/wx.h>
-#endif
-
-class App : public wxApp
-{
-public:
-    virtual bool OnInit();
-};
-
-class AppFrame : public wxFrame
-{
-public:
-    AppFrame();
-
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-};
-
-enum { ID_Hello = 1 };
+#include "app.h"
 
 wxIMPLEMENT_APP(App);
 
