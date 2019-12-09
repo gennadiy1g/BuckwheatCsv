301cc97e25a25f8bf38b6049da4e8b102fed8c90
Gennadiy <gennadiy1g@gmx.com>
2019-12-09 18:00:38 -0500

    Removed call to TokenizedFileLines::getPositionsOfSampleLines.
    class TokenizedFileLines is now RAII.

--- GridTable.cpp
+++ GridTable.cpp
@@ -3,7 +3,6 @@
 CsvFileGridTable::CsvFileGridTable()
 {
     mTokenizedFileLines = std::make_unique<TokenizedFileLines>(LR"^(C:\Users\genna_000\Documents\BuckwheatCsv\test data\Hits.csv)^");
-    mTokenizedFileLines->getPositionsOfSampleLines();
 }
 
 wxString CsvFileGridTable::GetValue(int row, int col)
