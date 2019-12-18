fd93a553274e4bc3aea7345e439d0564d1284ed4
Gennadiy <gennadiy1g@gmx.com>
2019-12-18 17:51:18 -0500

    Changed order of calls according to wxWidgets documentation.

--- OpenFileDialog.cpp
+++ OpenFileDialog.cpp
@@ -24,8 +24,11 @@ OpenFileDialog::OpenFileDialog(wxWindow* parent)
     separatorSizer->Add(new wxRadioButton(this, wxID_ANY, "Other"), sizerFlags);
 
     auto textCtrl = new wxTextCtrl();
-    textCtrl->Create(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(20, -1));
+    /* A window can be created initially disabled by calling wxWindow::Enable() on it before calling wxWindow::Create()
+     * to create the actual underlying window.
+     * See https://docs.wxwidgets.org/3.1.3/classwx_window.html#a4e933aa891f42fbb3b87438057c573af for the example */
     textCtrl->Enable(false);
+    textCtrl->Create(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(20, -1));
     separatorSizer->Add(textCtrl, sizerFlags);
 
     topSizer->Add(separatorSizer, wxSizerFlags(0).Border().Expand());
