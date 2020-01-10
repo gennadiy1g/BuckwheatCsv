gennadiy1g/BuckwheatCsv

commit cb79bb804fa9c25381e508f5f22fed5ab565dfb6

Check whether file scan failed. If yes - show empty grid.

main.cpp
@@ -138,10 +138,15 @@ void MainFrame::OnOpen(wxCommandEvent& event)
         }
         BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
 
-        mGrid->SetTable(mGridTable2.get());
-        mGridTable = std::move(mGridTable2);
-        BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
-        dynamic_cast<CsvFileGridTable*>(mGridTable.get())->setTokenizerParams(escape, separator, quote);
+        if (mScanFailed) {
+            mGridTable = std::make_unique<EmptyGridTable>();
+            mGrid->SetTable(mGridTable.get());
+        } else {
+            mGrid->SetTable(mGridTable2.get());
+            mGridTable = std::move(mGridTable2);
+            BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
+            dynamic_cast<CsvFileGridTable*>(mGridTable.get())->setTokenizerParams(escape, separator, quote);
+        }
     } else if (separator != mSeparator || quote != mQuote || escape != mEscape) {
         BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
         wxGridUpdateLocker gridUpdateLocker(mGrid);

commit dcf145b5bf42e1c7da89339a4b1cbd24d5d480b5

Fixed bug introduced by commit cb79bb804fa9c25381e508f5f22fed5ab565dfb6.

main.cpp
@@ -138,14 +138,12 @@ void MainFrame::OnOpen(wxCommandEvent& event)
         }
         BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
 
-        if (mScanFailed) {
-            mGridTable = std::make_unique<EmptyGridTable>();
-            mGrid->SetTable(mGridTable.get());
-        } else {
-            mGrid->SetTable(mGridTable2.get());
-            mGridTable = std::move(mGridTable2);
-            BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
-            dynamic_cast<CsvFileGridTable*>(mGridTable.get())->setTokenizerParams(escape, separator, quote);
+        mGrid->SetTable(mGridTable2.get());
+        mGridTable = std::move(mGridTable2);
+        BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
+        auto ptr = dynamic_cast<CsvFileGridTable*>(mGridTable.get());
+        if (ptr) {
+            ptr->setTokenizerParams(escape, separator, quote);
         }
     } else if (separator != mSeparator || quote != mQuote || escape != mEscape) {
         BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
@@ -172,6 +170,7 @@ wxThread::ExitCode MainFrame::Entry()
         mGridTable2 = std::make_unique<CsvFileGridTable>(bfs::path(mPath), std::bind(&MainFrame::OnProgress, this, std::placeholders::_1));
     } catch (const std::runtime_error& e) {
         BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
+        mGridTable2 = std::make_unique<EmptyGridTable>();
         mScanFailed = true;
         mErrorMessage = e.what();
     }