e180bfc7fcc5dbb411306f80d8583d146e9830f2
Gennadiy <gennadiy1g@gmx.com>
2020-04-17 16:22:34 -0400

    WIP on GoToColumnDialog.

--- GoToColumnDialog.cpp
+++ GoToColumnDialog.cpp
@@ -1,6 +1,15 @@
+#include <wx/combobox.h>
+
 #include "GoToColumnDialog.h"
 
 GoToColumnDialog::GoToColumnDialog(wxWindow* parent)
     : wxDialog(parent, wxID_ANY, "Go to Column")
 {
+    auto dialogSizer = new wxBoxSizer(wxVERTICAL);
+    dialogSizer->Add(new wxComboBox(this, wxID_ANY), wxSizerFlags(0).Expand().Border());
+
+    auto buttonSizer = CreateButtonSizer(wxOK | wxCANCEL);
+    dialogSizer->Add(buttonSizer, wxSizerFlags(0).Border().Center());
+
+    SetSizerAndFit(dialogSizer); // use the sizer for layout and set size and hints
 }
