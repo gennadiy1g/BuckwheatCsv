82309d2f87b5d130e00bb6e882f41054a38e785f
Gennadiy <gennadiy1g@gmx.com>
2020-04-17 16:36:24 -0400

    WIP on GoToColumnDialog.

--- GoToColumnDialog.cpp
+++ GoToColumnDialog.cpp
@@ -5,6 +5,9 @@
 GoToColumnDialog::GoToColumnDialog(wxWindow* parent)
     : wxDialog(parent, wxID_ANY, "Go to Column")
 {
+    auto& gLogger = GlobalLogger::get();
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
+
     auto dialogSizer = new wxBoxSizer(wxVERTICAL);
     dialogSizer->Add(new wxComboBox(this, wxID_ANY), wxSizerFlags(0).Expand().Border());
 
@@ -13,4 +16,6 @@ GoToColumnDialog::GoToColumnDialog(wxWindow* parent)
 
     SetIcon(wxICON(table_select_column));
     SetSizerAndFit(dialogSizer); // use the sizer for layout and set size and hints
+
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
 }
