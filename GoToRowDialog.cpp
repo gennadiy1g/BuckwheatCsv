b32b9a6cf396d1beecdf64bb7919e07875fc6975
Gennadiy <gennadiy1g@gmx.com>
2020-04-27 15:08:35 -0400

    Introduced class GoToRowDialog.

---
+++ GoToRowDialog.cpp
@@ -0,0 +1,19 @@
+#include "CsvTable/log.h"
+#include "GoToRowDialog.h"
+
+GoToRowDialog::GoToRowDialog(wxWindow* parent, GridTableBase* mGridTable)
+    : wxDialog(parent, wxID_ANY, "Go to Row")
+{
+    auto& gLogger = GlobalLogger::get();
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
+
+    auto dialogSizer = new wxBoxSizer(wxVERTICAL);
+
+    auto buttonSizer = CreateButtonSizer(wxOK | wxCANCEL);
+    dialogSizer->Add(buttonSizer, wxSizerFlags(0).Border().Center());
+
+    SetIcon(wxICON(table_select_column));
+    SetSizerAndFit(dialogSizer); // use the sizer for layout and set size and hints
+
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
+}
---
+++ GoToRowDialog.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <wx/filepicker.h>
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#include <wx/spinctrl.h>
+
+#include "GridTable.h"
+
+class GoToRowDialog : public wxDialog {
+public:
+    GoToRowDialog(wxWindow* parent, GridTableBase* mGridTable);
+    virtual ~GoToRowDialog() = default;
+
+    // Disallow assignment and pass-by-value.
+    GoToRowDialog(const GoToRowDialog& src) = delete;
+    GoToRowDialog& operator=(const GoToRowDialog& rhs) = delete;
+
+    // Explicitly default move constructor and move assignment operator.
+    GoToRowDialog(GoToRowDialog&& src) = default;
+    GoToRowDialog& operator=(GoToRowDialog&& rhs) = default;
+};
