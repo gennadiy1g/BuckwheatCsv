aa89e0ae3eaef402efa604087976d789ed10b93b
Gennadiy <gennadiy1g@gmx.com>
2020-05-14 17:31:16 -0400

    Added wxFLP_USE_TEXTCTRL to the style of wxFilePickerCtrl.
    wxFilePickerCtrl now works on Linux in the way very similar to how it
    works on Windows.

--- OpenFileDialog.cpp
+++ OpenFileDialog.cpp
@@ -60,7 +60,7 @@ OpenFileDialog::OpenFileDialog(wxWindow* parent, wxString path, wxChar separator
     auto dialogSizer = new wxBoxSizer(wxVERTICAL);
     auto filePickerCtrl
         = new wxFilePickerCtrl(this, wxID_ANY, wxEmptyString, "Open file", "Delimited text (*.csv;*.txt;*.tab)|*.csv;*.txt;*.tab",
-            wxDefaultPosition, wxDefaultSize, wxFLP_DEFAULT_STYLE | wxFLP_SMALL, FilePathValidator(mPath));
+            wxDefaultPosition, wxDefaultSize, wxFLP_DEFAULT_STYLE | wxFLP_USE_TEXTCTRL | wxFLP_SMALL, FilePathValidator(mPath));
     dialogSizer->Add(filePickerCtrl, wxSizerFlags(0).Expand().Border());
 
     auto separatorSizer = new wxStaticBoxSizer(wxHORIZONTAL, this, "Separator character");
@@ -119,14 +119,7 @@ OpenFileDialog::OpenFileDialog(wxWindow* parent, wxString path, wxChar separator
     Bind(wxEVT_RADIOBUTTON, &OpenFileDialog::OnEscapeRadioButton, this, ID_NO_ESCAPE, ID_OTHER_ESCAPE);
     Bind(wxEVT_FILEPICKER_CHANGED, &OpenFileDialog::OnFilePicker, this);
 
-    // Set focus to the filePickerCtrl
-    if (filePickerCtrl->HasTextCtrl()) {
-        filePickerCtrl->GetTextCtrl()->SetFocus();
-    } else {
-        // SetFocus() does not work for the filePickerCtrl under wxGTK, but the following trick does work
-        auto window = static_cast<wxWindow*>(filePickerCtrl);
-        window->Navigate();
-    }
+    filePickerCtrl->GetTextCtrl()->SetFocus();
     BOOST_LOG_SEV(gLogger, bltrivial::trace) << FUNCTION_FILE_LINE;
 }
 
