9d1ebdb993b4ca84f34fca2b4ad6a1add654ac32
Gennadiy <gennadiy1g@gmx.com>
2020-05-21 14:41:40 -0400

    Moved search control to the top of the dialog.

--- FindColumnDialog.cpp
+++ FindColumnDialog.cpp
@@ -11,6 +11,13 @@ FindColumnDialog::FindColumnDialog(wxWindow* parent)
 {
     auto dialogSizer = new wxBoxSizer(wxVERTICAL);
 
+    auto staticText = new wxStaticText(this, wxID_ANY, "Filter columns by name");
+    dialogSizer->Add(staticText, wxSizerFlags(0).Expand().Border());
+
+    auto searchCtrl = new wxSearchCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
+    searchCtrl->SetHint("few letters of a column's name, case insensitive, anywhere in the name");
+    dialogSizer->Add(searchCtrl, wxSizerFlags(0).Expand().Border());
+
     auto dataViewList = new wxDataViewListCtrl(this, wxID_ANY, wxDefaultPosition, wxSize(-1, 200));
     dataViewList->AppendTextColumn("#", wxDATAVIEW_CELL_INERT, -1, wxALIGN_RIGHT);
     dataViewList->AppendTextColumn("Name");
@@ -26,13 +33,6 @@ FindColumnDialog::FindColumnDialog(wxWindow* parent)
     dataViewList->AppendItem(data);
 
     dialogSizer->Add(dataViewList, wxSizerFlags(1).Expand().Border());
-
-    auto staticText = new wxStaticText(this, wxID_ANY, "Filter columns by name");
-    dialogSizer->Add(staticText, wxSizerFlags(0).Expand().Border(wxLEFT | wxRIGHT | wxTOP));
-
-    auto searchCtrl = new wxSearchCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
-    searchCtrl->SetHint("few letters of a column's name, case insensitive, anywhere in the name");
-    dialogSizer->Add(searchCtrl, wxSizerFlags(0).Expand().Border());
 
     auto buttonSizer = CreateButtonSizer(wxOK | wxCANCEL);
     dialogSizer->Add(buttonSizer, wxSizerFlags(0).Center().Border());
