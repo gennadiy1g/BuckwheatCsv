7c9d8b38886cee4f339e1a784d68d4d623e5f273
Gennadiy <gennadiy1g@gmx.com>
2020-05-27 14:48:55 -0400

    Increased version number from 1.0.2 to 1.1.

--- main.cpp
+++ main.cpp
@@ -125,7 +125,7 @@ void MainFrame::OnAbout(wxCommandEvent& event)
 {
     wxAboutDialogInfo aboutInfo;
     aboutInfo.SetName(wxTheApp->GetAppDisplayName());
-    aboutInfo.SetVersion("1.0.2");
+    aboutInfo.SetVersion("1.1");
     aboutInfo.SetDescription("Viewer for CSV files");
     aboutInfo.SetCopyright("(C) 2019-2020");
     aboutInfo.SetWebSite("https://github.com/gennadiy1g/BuckwheatCsv/releases");
